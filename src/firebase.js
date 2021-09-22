04d55d7142128eca8f97e0ec2e5d2fac9169ecb6
Ayush Singh <82327294+ayush-321@users.noreply.github.com>
2021-09-22 14:37:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,9 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA7M0jWcem-swqPS-MaHIKPpy8HwR4v5pw",
-    authDomain: "clone-d2fbf.firebaseapp.com",
-    projectId: "clone-d2fbf",
-    storageBucket: "clone-d2fbf.appspot.com",
-    messagingSenderId: "801038024451",
-    appId: "1:801038024451:web:08e0a537169a210f2af336",
-    measurementId: "G-QWE4DX5LJ7"
-  };
+  apiKey: "AIzaSyA7M0jWcem-swqPS-MaHIKPpy8HwR4v5pw",
+  authDomain: "clone-d2fbf.firebaseapp.com",
+  projectId: "clone-d2fbf",
+  storageBucket: "clone-d2fbf.appspot.com",
+  messagingSenderId: "801038024451",
+  appId: "1:801038024451:web:08e0a537169a210f2af336",
+  measurementId: "G-QWE4DX5LJ7",
+};
