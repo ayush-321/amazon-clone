994d985bc531c8931142fdc1e35a5528260428b7
Ayush Singh <82327294+ayush-321@users.noreply.github.com>
2021-09-23 12:49:19 +0530

    Built Payment Component UI

--- src/Checkout/Subtotal.js
+++ src/Checkout/Subtotal.js
@@ -3,8 +3,10 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "../React Context API/StateProvider";
 import { getBasketTotal } from "../React Context API/reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
+  const history = useHistory();
   const [{ basket }, dispatch] = useStateValue();
 
   return (
@@ -27,7 +29,9 @@ function Subtotal() {
         prefix={"₹"}
       />
 
-      <button>Proceed to Checkout</button>
+      <button onClick={(e) => history.push("/payment")}>
+        Proceed to Checkout
+      </button>
     </div>
   );
 }
